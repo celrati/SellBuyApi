7e88750e241ac39daebc70c3ff637b7479cb50a9
Charif Achraf <achraf.charif@safety-line.fr>
2022-03-07 15:22:52 +0100

    integrate swagger

--- target/classes/create.sql
+++ target/classes/create.sql
@@ -24,3 +24,51 @@ alter table users add constraint UKr43af9ap4edm43mmtq01oddj6 unique (username)
 alter table users add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email)
 alter table user_roles add constraint FKh8ciramu9cc9q3qcqiv4ue8a6 foreign key (role_id) references roles (id)
 alter table user_roles add constraint FKhfh9dx7w3ubf1co1vdev94g3f foreign key (user_id) references users (id)
+create table roles (id bigint not null auto_increment, name varchar(60), primary key (id)) engine=InnoDB
+create table user_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id)) engine=InnoDB
+create table users (id bigint not null auto_increment, email varchar(50), name varchar(50), password varchar(100), username varchar(50), primary key (id)) engine=InnoDB
+alter table roles add constraint UK_nb4h0p6txrmfc0xbrd1kglp9t unique (name)
+alter table users add constraint UKr43af9ap4edm43mmtq01oddj6 unique (username)
+alter table users add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email)
+alter table user_roles add constraint FKh8ciramu9cc9q3qcqiv4ue8a6 foreign key (role_id) references roles (id)
+alter table user_roles add constraint FKhfh9dx7w3ubf1co1vdev94g3f foreign key (user_id) references users (id)
+create table roles (id bigint not null auto_increment, name varchar(60), primary key (id)) engine=InnoDB
+create table user_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id)) engine=InnoDB
+create table users (id bigint not null auto_increment, email varchar(50), name varchar(50), password varchar(100), username varchar(50), primary key (id)) engine=InnoDB
+alter table roles add constraint UK_nb4h0p6txrmfc0xbrd1kglp9t unique (name)
+alter table users add constraint UKr43af9ap4edm43mmtq01oddj6 unique (username)
+alter table users add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email)
+alter table user_roles add constraint FKh8ciramu9cc9q3qcqiv4ue8a6 foreign key (role_id) references roles (id)
+alter table user_roles add constraint FKhfh9dx7w3ubf1co1vdev94g3f foreign key (user_id) references users (id)
+create table roles (id bigint not null auto_increment, name varchar(60), primary key (id)) engine=InnoDB
+create table user_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id)) engine=InnoDB
+create table users (id bigint not null auto_increment, email varchar(50), name varchar(50), password varchar(100), username varchar(50), primary key (id)) engine=InnoDB
+alter table roles add constraint UK_nb4h0p6txrmfc0xbrd1kglp9t unique (name)
+alter table users add constraint UKr43af9ap4edm43mmtq01oddj6 unique (username)
+alter table users add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email)
+alter table user_roles add constraint FKh8ciramu9cc9q3qcqiv4ue8a6 foreign key (role_id) references roles (id)
+alter table user_roles add constraint FKhfh9dx7w3ubf1co1vdev94g3f foreign key (user_id) references users (id)
+create table roles (id bigint not null auto_increment, name varchar(60), primary key (id)) engine=InnoDB
+create table user_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id)) engine=InnoDB
+create table users (id bigint not null auto_increment, email varchar(50), name varchar(50), password varchar(100), username varchar(50), primary key (id)) engine=InnoDB
+alter table roles add constraint UK_nb4h0p6txrmfc0xbrd1kglp9t unique (name)
+alter table users add constraint UKr43af9ap4edm43mmtq01oddj6 unique (username)
+alter table users add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email)
+alter table user_roles add constraint FKh8ciramu9cc9q3qcqiv4ue8a6 foreign key (role_id) references roles (id)
+alter table user_roles add constraint FKhfh9dx7w3ubf1co1vdev94g3f foreign key (user_id) references users (id)
+create table roles (id bigint not null auto_increment, name varchar(60), primary key (id)) engine=InnoDB
+create table user_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id)) engine=InnoDB
+create table users (id bigint not null auto_increment, email varchar(50), name varchar(50), password varchar(100), username varchar(50), primary key (id)) engine=InnoDB
+alter table roles add constraint UK_nb4h0p6txrmfc0xbrd1kglp9t unique (name)
+alter table users add constraint UKr43af9ap4edm43mmtq01oddj6 unique (username)
+alter table users add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email)
+alter table user_roles add constraint FKh8ciramu9cc9q3qcqiv4ue8a6 foreign key (role_id) references roles (id)
+alter table user_roles add constraint FKhfh9dx7w3ubf1co1vdev94g3f foreign key (user_id) references users (id)
+create table roles (id bigint not null auto_increment, name varchar(60), primary key (id)) engine=InnoDB
+create table user_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id)) engine=InnoDB
+create table users (id bigint not null auto_increment, email varchar(50), name varchar(50), password varchar(100), username varchar(50), primary key (id)) engine=InnoDB
+alter table roles add constraint UK_nb4h0p6txrmfc0xbrd1kglp9t unique (name)
+alter table users add constraint UKr43af9ap4edm43mmtq01oddj6 unique (username)
+alter table users add constraint UK6dotkott2kjsp8vw4d0m25fb7 unique (email)
+alter table user_roles add constraint FKh8ciramu9cc9q3qcqiv4ue8a6 foreign key (role_id) references roles (id)
+alter table user_roles add constraint FKhfh9dx7w3ubf1co1vdev94g3f foreign key (user_id) references users (id)
